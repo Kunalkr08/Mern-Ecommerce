22109592a4ed8d2650abe6507727385baff6cc63
Kunal Kumar <102505903+Kunalkr08@users.noreply.github.com>
2024-05-31 11:27:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors({
-    origin : https://mern-ecommerce-6exy.vercel.app,
+    origin : true,
     methods: ["GET", "POST", "PUT", "UPDATE", "DELETE"],
     credentials : true
 })); 
